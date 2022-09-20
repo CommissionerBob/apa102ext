154e58e8e07dcbcf32fdf69f85214d0ddad253e9
CommissionerBob <rnks@mac.com>
2022-09-20 12:24:27 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-let vol = 0
-let strip = APA102.createStrip(8, PixelMode.RGB)
-strip.setPin(DigitalPin.P1, DigitalPin.P0)
-strip.setBrightness(200)
-basic.forever(function () {
-    vol = Math.map(input.soundLevel(), 0, 150, 0, 360)
-    strip.showColor(APA102.toCol(vol, 255, 50))
-})
+
