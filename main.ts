976fca89cf1cbc9a0f4e8e9cb3b4575663cc6f3b
CommissionerBob <rnks@mac.com>
2022-09-28 13:42:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-let strip = apa102.createStrip(60, PixelMode.RGB)
-let range2 = strip.range(0, 2)
-strip.setPin(DigitalPin.P1, DigitalPin.P0)
-range2 = strip.range(10, 30)
-range2.showbow(1, 180)
-for (let index = 0; index <= 20; index++) {
-    range2 = strip.range(10, 30 - index)
-    range2.clear()
-    range2.showbow(1, 180)
-    basic.pause(1)
-}
+
